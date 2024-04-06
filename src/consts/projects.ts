d93319b19dd6e88a0f0969a66ff361e5dd623782
MartL7 <latiapelitos123@gmail.com>
2024-04-05 18:37:40 -0600

    🤍 Change images for production

--- src/consts/projects.ts
+++ src/consts/projects.ts
@@ -20,7 +20,7 @@ export const projects: Project[] = [
   {
     name: 'Fichas CBTis 86',
     description: 'Aplicación web para la gestión de fichas de inscripción de alumnos del CBTis 86, con la finalidad de agilizar el proceso de inscripción de los alumnos. Creada con vanilla JavaScript, PHP, Mysql.',
-    image: '/public/img/img-projects/fichas.webp',
+    image: '/img/img-projects/fichas.webp',
     tags: [
       { name: 'Html', icon: 'fab fa-html5 text-orange-500 text-2xl sm:text-2xl' },
       { name: 'Css', icon: 'fab fa-css3 text-blue-500 text-2xl sm:text-2xl' },
@@ -33,7 +33,7 @@ export const projects: Project[] = [
   {
     name: 'Control de Créditos de Clientes',
     description: 'Aplicación web para control de Créditos de Clientes de la empresa LOG-IN, con la finalidad de llevar un control de los créditos otorgados a los clientes, así como de los pagos realizados por los mismos. Creada con Astro, TypeScript y Tailwind CSS.',
-    image: '/public/img/img-projects/credits-log-in.webp',
+    image: '/img/img-projects/credits-log-in.webp',
     tags: [
       { name: 'Astro', icon: AstroIcon },
       { name: 'TypeScript', icon: TypeScriptIcon },
@@ -44,7 +44,7 @@ export const projects: Project[] = [
   {
     name: 'Tablas de Estadísticas',
     description: 'Tablas de Distribución de Frecuencias, con la finalidad de mostrar la información de una manera más visual y entendible. Creada con React, JavaScript y Tailwind CSS.',
-    image: '/public/img/img-projects/tablas-estadistica.webp',
+    image: '/img/img-projects/tablas-estadistica.webp',
     tags: [
       { name: 'React', icon: 'fab fa-react text-blue-400 text-2xl sm:text-2xl' },
       { name: 'JavaScript', icon: 'fab fa-js-square text-yellow-500 text-2xl sm:text-3xl' },

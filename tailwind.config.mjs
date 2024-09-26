acece7f087bde9248eb803dc9d7a272fdea8c767
MartL7 <latiapelitos123@gmail.com>
2024-09-26 00:10:47 -0600

    😻 Changed tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'ubuntu-mono': ['Ubuntu Mono', 'monospace'],
-			},
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'ubuntu-mono': ['Ubuntu Mono', 'monospace']
+      },
+      colors: {
+        primary: '#0099FF',
+        secondary: '#DEF2FF',
+        page: '#1E1421'
+      }
+    }
+  },
+  plugins: []
 }
